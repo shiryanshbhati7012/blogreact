fa37be42dee610d85571666853a6945aa0f445dd
shiryanshbhati7012 <shiryansh7012@gmail.com>
2023-10-08 16:30:16 +0530

    bolrt

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
-import memories from './images/memories.png';
+import memories from './images/memories.jpg';
 
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
